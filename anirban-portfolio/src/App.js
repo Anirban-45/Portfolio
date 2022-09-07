f2003fea7998999fc7a554e700dc608a83bd72a3
Anirban-45 <azadarannya@gmail.com>
2022-09-07 17:17:33 +0600

    Refix

--- anirban-portfolio/src/App.js
+++ anirban-portfolio/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react' ;
+import React from 'react'
 
 function App() {
-    return(
-        <div className = "App">
-            <h1>Hello</h1>
-        </div>
-    );
+  return (
+    <div></div>
+  );
 }
-export default App ;
+
+export default App;
