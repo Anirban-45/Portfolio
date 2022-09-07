696e743ac7cf1916369c3adbb69ffbfe17917949
Anirban-45 <azadarannya@gmail.com>
2022-09-08 01:36:12 +0600

    folder structure refix

---
+++ anirban-portfolio/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Navbar.min.css'
+
+function Navbar() {
+    return(
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    TRVL
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
