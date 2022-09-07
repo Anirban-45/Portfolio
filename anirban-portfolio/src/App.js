f1633a92923f5f6b9351b618f4eaefaae5c7b214
Anirban-45 <azadarannya@gmail.com>
2022-09-08 03:59:35 +0600

    Initiated SCSS

--- anirban-portfolio/src/App.js
+++ anirban-portfolio/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import Navbar from './components/Navbar';
+import './styles/App.css'
 
 function App() {
     return (
