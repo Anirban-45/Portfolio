02e558978a7ed60712c40a8779874e65e7b470f0
Anirban-45 <azadarannya@gmail.com>
2022-09-11 19:10:26 +0600

    Footer Almost completed

--- anirban-portfolio/src/components/Footer.js
+++ anirban-portfolio/src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../styles/Footer.css'
+import {AiFillInstagram} from 'react-icons/ai'
+import {AiFillLinkedin} from 'react-icons/ai'
+import {FaDeviantart} from 'react-icons/fa'
+import {AiFillGithub} from 'react-icons/ai'
 
 function Footer(props) {
     return(
@@ -15,8 +19,35 @@ function Footer(props) {
                     <div className="circle"></div>
                 </div>
             </div>
-            <div>
-
+            <div className="footer-bottom">
+                <div className="footer-left">
+                    <span className="others-text">Others</span>
+                    <div className="other-links">
+                        <span>Back to Top</span>
+                        <span>Writeups</span>
+                        <a href="https://drive.google.com/file/d/1T22NHodPkOWQPcqmlv9B5qr0I8ViHvAb/view?usp=sharing">
+                            <span>Resume</span>
+                        </a>
+                    </div>
+                </div>
+                <div className="footer-right">
+                    <span className="social-text">Socials</span>
+                    <div className="icons">
+                        <a href="https://www.instagram.com/qitzuphyn/">
+                            <AiFillInstagram className="icon"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/anirban-tasfin-azad-36177916a/">
+                            <AiFillLinkedin className="icon"/>
+                        </a>
+                        <a href="https://www.deviantart.com/onirban2">
+                            <FaDeviantart className="icon"/>
+                        </a>
+                        <a href="https://github.com/Anirban-45">
+                            <AiFillGithub className="icon"/>
+                        </a>
+                    </div>
+                    <span className="rights">©2022 all rights reserved</span>
+                </div>
             </div>
         </div>
     )
