feb0587114e242fb39baf829e8bd4d864a76dc9e
Anirban-45 <azadarannya@gmail.com>
2022-09-15 21:10:51 +0600

    Contact screen complete

--- anirban-portfolio/src/screens/Contact.js
+++ anirban-portfolio/src/screens/Contact.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Contactform from '../components/Contactform';
+import '../styles/Contact.css'
 
 function Contact() {
     return(
-        <Contactform/>
+        <div className='Ccontainer'>
+            <div className="top-welcome">
+                <label  id="top-label">Want to get in touch?</label>
+                <label id="top-label">Drop me a letter!</label>
+            </div>
+            <Contactform/>
+        </div>
     )
 }
 
